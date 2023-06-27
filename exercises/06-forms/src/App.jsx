2a3c6e68814a0ccbeb9e2152a0eaba51a7d301b0
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-06-26 20:42:26 -0400

    submit ex 06 forms

--- exercises/06-forms/src/App.jsx
+++ exercises/06-forms/src/App.jsx
@@ -1,15 +1,45 @@
 import "./App.css";
+
 // Import here
+import { useState } from "react";
+import statesList from "./assets/states.json";
+import countriesList from "./assets/countries.json";
 
 function App() {
+  const [formData, setFormData] = useState({
+    country: "United States",
+    state: "NY",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleChange = (event) => {
+    console.table(formData);
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.table(formData);
+  };
+  const handleCheckbox = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.checked });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
   return (
-    <form className="container mt-4" method="POST">
+    <form
+      onSubmit={handleSubmit}
+      className="container mt-4"
+      method="POST"
+    >
       {/* You will need to handle form submission */}
       <div className="mb-3">
-        <label htmlFor="firstName" className="control-label">
+        <label
+          htmlFor="firstName"
+          className="control-label"
+        >
           First Name
         </label>
         <input
+          onChange={handleChange}
+          value={formData.firstName}
           id="firstName"
           name="firstName"
           type="text"
@@ -17,10 +47,15 @@ function App() {
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="lastName" className="control-label">
+        <label
+          htmlFor="lastName"
+          className="control-label"
+        >
           Last Name
         </label>
         <input
+          onChange={handleChange}
+          value={formData.lastName}
           id="lastName"
           name="lastName"
           type="text"
@@ -28,10 +63,15 @@ function App() {
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="addressLine1" className="control-label">
+        <label
+          htmlFor="addressLine1"
+          className="control-label"
+        >
           Address Line 1
         </label>
         <input
+          onChange={handleChange}
+          value={formData.addressLine1}
           id="addressLine1"
           name="addressLine1"
           type="text"
@@ -43,24 +83,59 @@ function App() {
       </div>
 
       <div className="mb-3">
-        <label htmlFor="city" className="control-label">
+        <label
+          htmlFor="city"
+          className="control-label"
+        >
           City / Town
         </label>
-        <input id="city" name="city" type="text" className="form-control" />
+        <input
+          onChange={handleChange}
+          value={formData.city}
+          id="city"
+          name="city"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="state" className="control-label">
+        <label
+          htmlFor="state"
+          className="control-label"
+        >
           State / Province / Region
         </label>
         {/* Loop through the states you imported here */}
-        <select id="state" name="state" className="form-control" />
+        <select
+          onChange={handleChange}
+          value={formData.state}
+          id="state"
+          name="state"
+          className="form-control"
+        >
+          {statesList.map((item) => {
+            return (
+              <option
+                key={`state-${item}`}
+                value={item}
+              >
+                {item}
+              </option>
+            );
+          })}
+        </select>
       </div>
 
       <div className="mb-3">
-        <label htmlFor="postalCode" className="control-label">
+        <label
+          htmlFor="postalCode"
+          className="control-label"
+        >
           Zip / Postal Code
         </label>
         <input
+          onChange={handleChange}
+          value={formData.postalCode}
           id="postalCode"
           name="postalCode"
           type="text"
@@ -69,24 +144,60 @@ function App() {
       </div>
 
       <div className="mb-3">
-        <label htmlFor="country" className="control-label">
+        <label
+          htmlFor="country"
+          className="control-label"
+        >
           Country
         </label>
         {/* Loop through the countries you imported here */}
-        <select id="country" name="country" className="form-control" />
+        <select
+          onChange={handleChange}
+          value={formData.country}
+          id="country"
+          name="country"
+          className="form-control"
+        >
+          {countriesList.map((item) => {
+            return (
+              <option
+                key={`country-${item}`}
+                value={item}
+              >
+                {item}
+              </option>
+            );
+          })}
+        </select>
       </div>
       <div className="mb-3 form-check">
         <input
+          onChange={handleCheckbox}
           id="signUpForNewsLetter"
           name="signUpForNewsLetter"
           type="checkbox"
           className="form-check-input"
         />
-        <label htmlFor="signUpForNewsLetter" className="form-check-label">
+        <label
+          htmlFor="signUpForNewsLetter"
+          className="form-check-label"
+        >
           Sign Up For Newsletter
         </label>
       </div>
-      <button type="submit" className="btn btn-primary">
+      <div
+        className={`alert ${
+          formData.signUpForNewsLetter ? "alert-success" : "alert-danger"
+        }`}
+      >
+        {formData.signUpForNewsLetter
+          ? "Thank you for signing up for our newsletter!"
+          : "Please sign up for our newsletter!"}
+      </div>
+      <button
+        type="submit"
+        className="btn btn-primary"
+      >
         Submit
       </button>
 
@@ -94,11 +205,13 @@ function App() {
        * Find a way to only display this once the form has been submitted.
        * Hint: You will need to change "false" below with something else
        */}
-      {false && (
+      {isSubmitted && (
         <div className="card card-body bg-light mt-4 mb-4">
           Results:
           <ul className="list-unstyled mb-0">
-            {/* Add <li></li> tags here */}
+            {Object.values(formData).map((item) => {
+              return <li key={item}>{item}</li>;
+            })}
           </ul>
         </div>
       )}
