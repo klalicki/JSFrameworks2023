5e15dde68e976ed07c092c9b8475d99ca09ea3a2
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-10 18:06:56 -0400

    refactored AddressResults to add a function for niceVal instead of inline

--- exercises/08-lifting-state-up/src/components/AddressResults/AddressResults.jsx
+++ exercises/08-lifting-state-up/src/components/AddressResults/AddressResults.jsx
@@ -10,24 +10,24 @@
  * - country
  */
 function AddressResults({ formData, setDisplayResults }) {
+  const niceVal = (value) => {
+    if (typeof value === "boolean") {
+      return value ? "true" : "false";
+    } else {
+      return value;
+    }
+    // nested ternary - functional but hard to read
+    // const niceVal =
+    //   key === "confirm" ? (value ? "true" : "false") : value;
+  };
   return (
     <div className="card card-body bg-light mt-4 mb-4">
       Results:
       <ul className="list-unstyled mb-0">
         {Object.entries(formData).map(([key, value]) => {
-          // format the boolean value to be a nice string
-          let niceVal;
-          if (typeof value === "boolean") {
-            niceVal = value ? "true" : "false";
-          } else {
-            niceVal = value;
-          }
-          // nested ternary - functional but hard to read
-          // const niceVal =
-          //   key === "confirm" ? (value ? "true" : "false") : value;
           return (
             <li key={key}>
-              {key}:{niceVal}
+              {key}: {niceVal(value)}
             </li>
           );
         })}
